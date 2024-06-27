f5ac75702e589c0b0f8a29c22a9536e9db816452
dangthanhphuc <dangthanhphuc2512@gmail.com>
2024-06-27 15:44:24 +0700

    Add header task complete

---
+++ src/app/responses/user.response.ts
@@ -0,0 +1,15 @@
+
+export interface UserResponse {
+
+    id : number;
+    name: string;
+    phone_number : string;
+    img_url : string;
+    email : string;
+    date_of_birth : Date;
+    username : string;
+    password : string;
+    licence : string; 
+    role : string;
+
+}
