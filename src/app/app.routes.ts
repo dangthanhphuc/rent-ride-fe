88b4114fb0f0cebaccd6d8da4aa03db4a9c13852
tahoangphuoc <tahoangphuocbp@gmail.com>
2024-07-02 10:50:59 +0700

    add layout account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { AccountComponent } from './components/account/account.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/homepage', pathMatch: 'full'},
     {path: "homepage", component: HomepageComponent},
-
+    {path: "account", component: AccountComponent},
     {path: "**", component: PageNotFoundComponent}
 ];
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,4 +12,5 @@ export class FooterComponent {
   faFacebook = faFacebook;
   faTiktok = faTiktok;
   faTwitter = faTwitter;
+  
 }
