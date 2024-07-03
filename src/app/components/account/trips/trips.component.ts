2ba34040a458557213fb7d6cf4c877e580560489
NguyenSang <nguyentansangit2411@gmail.com>
2024-07-03 14:11:39 +0700

    Trips

--- src/app/components/account/trips/trips.component.ts
+++ src/app/components/account/trips/trips.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-trips',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './trips.component.html',
   styleUrl: './trips.component.scss'
 })
-export class TripsComponent {
+export class TripsComponent implements OnInit {
+  faFilter = faFilter;
+  ngOnInit(): void {
+    document.addEventListener('DOMContentLoaded', () => {
+      const filterButton = document.querySelector('.button-filter') as HTMLElement;
+      const filterModal = document.querySelector('.filter-modal') as HTMLElement;
+      const closeFilterButton = document.getElementById('closeFilterButton') as HTMLElement;
+      filterModal.style.display = 'none';
 
+      filterButton.addEventListener('click', () => {
+        filterModal.style.display = 'block';
+      });
+
+      closeFilterButton.addEventListener('click', () => {
+        filterModal.style.display = 'none';
+      });
+    });
+  }
 }
