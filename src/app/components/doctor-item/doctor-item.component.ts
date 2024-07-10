6ad9ff4d83d1cf9f88e6883c5bd9666a7c18bd2a
dangthanhphuc <dangthanhphuc2512@gmail.com>
2024-07-10 22:01:29 +0700

    The first commit

---
+++ src/app/components/doctor-item/doctor-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctor-item.component.html',
+  styleUrl: './doctor-item.component.scss'
+})
+export class DoctorItemComponent {
+
+}
